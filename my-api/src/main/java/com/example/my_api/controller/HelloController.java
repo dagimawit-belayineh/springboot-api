08a6bc835854f0dc3b77d7807dfdea62a09a8922
dagimawit-belayineh <dagimawitbelayineh@gmail.com>
2025-10-14 09:13:01 +0300

    spring API with stable version

---
+++ my-api/src/main/java/com/example/my_api/controller/HelloController.java
@@ -0,0 +1,10 @@
+package com.example.my_api.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HelloController{
+    @GetMapping("/api/hello")
+    public String hello(){
+        return "Hello Spring Boot Api";
+    }
+}
